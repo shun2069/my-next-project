40cafcf23baa9dc439e7a3e9fd3abed977801055
小川 瞬 <syun.ogawa0609@gmail.com>
2024-11-26 17:08:04 +0900

    3章まで完了

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,70 @@
+import styles from "./page.module.css";
+import Image from "next/image";
 
+import NewsList from "@/app/_compornents/Newslist";
+import ButtonLink from "@/app/_compornents/ButtonLink";
+import {News} from "@/app/_libs/microcms";
+
+const data: {
+  contents: News[];
+} = {
+  contents: [
+    {
+      id: "1",
+      title: "渋谷にオフィスを移転しまた",
+      category: {
+        name: "更新情報",
+      },
+      publishedAt: "2023/05/19",
+      createdAt: "2023/03/23",
+    },
+    {
+      id: "2",
+      title: "当社CEOが業界のトップリーダーに選出されました",
+      category: {
+        name: "更新情報",
+      },
+      publishedAt: "2023/05/19",
+      createdAt: "2023/03/23",
+    },
+    {
+      id: "3",
+      title: "テストの記事です",
+      category: {
+        name: "更新情報",
+      },
+      publishedAt: "2023/05/19",
+      createdAt: "2023/03/23",
+    },
+  ],
+};
 
 export default function Home() {
-  const name = "世界";
+const sliceData = data.contents.slice(0, 2);
   return (
-    <div>
-    <h1 className="{styles.title}">テクノロジーの力で{name}を変える</h1><p className="{styles.description}">私たちは市場をリードしているグローバルテックカンパニーです</p>
-    </div>
+    <>
+      <section className={styles.top}>
+        <div>
+          <h1 className={styles.title}>テクノロジーの力で日本を変える</h1>
+          <p className={styles.description}>
+            私たちは市場をリードしているグローバルテックカンパニーです
+          </p>
+        </div>
+        <Image
+          className={styles.bgimg}
+          src="/img-mv.jpg"
+          alt=""
+          width={4000}
+          height={1200}
+        />
+      </section>
+      <section className={styles.news}>
+        <h2 className={styles.newsTitle}>News</h2>
+       <NewsList news={sliceData} />
+        <div className={styles.newsLink}>
+          <ButtonLink href="/news">もっと見る</ButtonLink>
+        </div>
+      </section>
+    </>
   );
 }
